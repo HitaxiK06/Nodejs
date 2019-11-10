4e8e5a1e8402fc4578d7213537604d951a06a916
HitaxiK06 <hitaxi.kachhadiya@synoverge.com>
2019-11-10 23:48:20 +0530

    commit

--- weather-app/src/exercise.js
+++ weather-app/src/exercise.js
@@ -1,22 +1,42 @@
+//packages
 const express= require('express')
 const path = require('path')
 
-
+//declaration
 const app = express()
 const publicdirectorypath = path.join(__dirname,'../public')
-
+const viewpath = path.join(__dirname,'../templates')
+app.set('view engine','hbs')
+app.set('views',viewpath)
 app.use(express.static(publicdirectorypath))
+
+
 app.get('',(req,res)=>{
-    res.send('<h1>Home Page of Exercise</h1>')
+    res.render('index',{
+        title : 'Home',
+        age:29
+    })
+    //res.send('<h1>Home Page of Exercise</h1>')
 })
 app.get('/calculator',(req,res)=>{
-    res.send({
-        name : 'Hitaxi',
+    res.render('calculator',{
+        title : 'Calculator',
+        age:29
+    })
+    // res.send({
+    //     title : 'calculator',
+    //     age:29
+    // })
+})
+app.get('/student',(req,res)=>{
+    res.render('student',{
+        title : 'Student',
         age:29
     })
 })
-app.get('/student',(req,res)=>{
-    res.send('Crud Oper')
+
+app.get('*',(req,res)=>{
+    res.render('404')
 })
 app.listen(3000,()=>{
     console.log('server is up')
