a8885a50c7046623df84e1eb39b117b24da87abd
HitaxiK06 <hitaxi.kachhadiya@synoverge.com>
2019-11-10 20:54:47 +0530

    validation change

--- Filestream/2-validation.js
+++ Filestream/2-validation.js
@@ -2,3 +2,4 @@ const validator = require('validator')
 const chalk = require('chalk')
 
 console.log(chalk.blue.inverse.bold(validator.isEmail('Hitaxi.Kachhadiya@synvergetech.com')))
+console.log(chalk.red.inverse.bold(validator.isEmail('Hitaxi.Kachhadiya.com')))
