87ea7a0a819d9fd9ec107e0ff25eca08786fa241
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-11 12:04:00 +0530

    commit

--- chalk/chalks.js
+++
@@ -1,4 +0,0 @@
-const chalk=require('chalk')
-
-console.log("Text in green color:: "+chalk.green("Green Text"))
-console.log("Bold color :: "+chalk.green.inverse.bold("Green Text"))
--- validation/validation.js
+++
@@ -1,5 +0,0 @@
-const chalk =require('chalk')
-const validator =require('validator')
-
-console.log("Email Validation ::hitaxi@gmail.com :- "+chalk.green(validator.isEmail('hitaxi@gmail.com')))
-console.log("Email Validation :: hitaxi.com:-"+chalk.red(validator.isEmail('hitaxi.com')))
