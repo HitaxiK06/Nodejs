46c472b563dba09c0d005d55fa2d8f7ca6614f12
HitaxiK06 <hitaxi.kachhadiya@synoverge.com>
2019-11-11 21:28:19 +0530

    commit

--- Yargs/Notes.js
+++ Yargs/Notes.js
@@ -1,24 +1,35 @@
 const fs = require('fs')
 
+const addNote=function(title){
+    const notes=loadnotes()
+    const duplicateNotes=notes.filter(function(note){
+        return note.title ===title
+    })
+    if(duplicateNotes.length===0)
+    {
+        notes.push({
+            title:title
+        })
+        saveNotes(notes)
+    }
+         
+}
+
 const removeNote=function(title){
     const notes=loadnotes()
     const duplicateNotes=notes.filter(function(note){
         return note.title !==title
     })
-debugger
-     if(duplicateNotes===0){
-        notes.push({
-            title: title
-        })
-       
          saveNotes(duplicateNotes)
-       
-   }
+      
+   
 }
+
 const saveNotes = function(notes){
     const datajson = JSON.stringify(notes)
     fs.writeFileSync('Notes.json',datajson)
 }
+
 const loadnotes = function(){
     try{
         const databuffer = fs.readFileSync('Notes.json')
@@ -27,7 +38,9 @@ const loadnotes = function(){
     }
     catch(e){return []}
 }
+
 module.exports={
     removeNote: removeNote,
+    addNote:addNote,
     loadnotes: loadnotes
 }
--- Yargs/yargs.js
+++ Yargs/yargs.js
@@ -13,6 +13,7 @@ yargs.command({
         }
     },
     handler: function(argv){
+        notes.addNote(argv.title)
         console.log('Add New notes'+argv.title)
     }
 })
