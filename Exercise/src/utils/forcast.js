ad59b7398c104c807ab495474d9d24ef7370a0e3
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-18 18:27:49 +0530

    commit

---
+++ Exercise/src/utils/forcast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forcast =(longitude,latitude,location,callback)=>{
+    const url='https://api.darksky.net/forecast/6f02e60e249e5e921d56cbcbc8a4cfaf/'+longitude+','+latitude+''
+
+request({url, json: true}, function (error, response) {
+  if(error){
+    callback('error:', error);
+  }
+  else if(response.body.error){
+    callback('unable to find location');
+  }
+  else{
+  //console.log('statusCode:', response ); 
+  callback(undefined,response.body.daily.data[0].summary +'Current Temp is: ' + response.body.currently.temperature + ' and chances of rain is: '+response.body.currently.precipProbability);
+}});
+}
+
+module.exports = forcast
