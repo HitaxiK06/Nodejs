e0265218d33e99a6160048526c3706ce3570569c
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-19 12:38:02 +0530

    commit

--- Exercise/public/js/app.js
+++ Exercise/public/js/app.js
@@ -5,3 +5,12 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
         console.log(data)
     })
 })
+fetch('http://localhost:3000/weather?address=Ahmedabad').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){console.log(data.error)}
+        else
+        {
+            console.log(data.location)
+        }
+    })
+})
