5fbef3c10bb437465b77250eee2d73063bd64b9a
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-19 17:57:11 +0530

    commit

--- Mongo-db/mongodb.js
+++ Mongo-db/mongodb.js
@@ -4,6 +4,15 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL,{ useNewUrlParser: true }, (error,client) =>{
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+         return console.log('unable to connect Database')
+    }
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name: 'Hitaxi Kachhadiya',
+        age: 21
+    })
 
+    console.log('Connection Done')
 })
