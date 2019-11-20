20d974e772491c2399399833916f252eb8cda176
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-20 17:15:18 +0530

    commit

---
+++ Mongo-db/src/db/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const user= new User({
+    name:'',
+    age:''
+})
