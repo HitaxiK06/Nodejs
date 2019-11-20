be7eb24f6bdc851089b74e6c732826f99ce99d1a
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-20 13:21:46 +0530

    commit

--- Filestream/4-callback.js
+++ Filestream/4-callback.js
@@ -1,31 +1,33 @@
-setTimeout(()=>{
+setTimeout(() => {
     console.log('print after 2 sec')
+}, 2000)
 
-},2000)
 
-
-const geocode =(address,callback)=>{
-setTimeout(()=>{
-    const data={
-        latitude:0,
-        longitude:0
-    }
-    callback(data)
-},2000)
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        }
+        callback(data)
+    }, 2000)
 }
 
-geocode('Ahmedabad',(data)=>{
+geocode('Ahmedabad', (data) => {
     console.log(data)
 })
 
-const add =(a,b,callback)=>{
-    setTimeout(()=>{
+const add = (a, b, callback) => {
+    setTimeout(() => {
         const sum = a + b
         callback(sum)
-    },2000)
+    }, 2000)
 }
-    
-add(1,2,(sum)=>{
+
+add(1, 2, (sum) => {
     console.log(sum)
 })
-    
+
+
+
+
