fae6b886071f1c38036bf137f4ba2e538c6919b9
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-21 18:01:42 +0530

    commit

--- Mongo-db/src/db/connection_sql.js
+++ Mongo-db/src/db/connection_sql.js
@@ -1,35 +1,35 @@
 const mssql = require('mssql')
-//const express = require('express');
+const express = require('express');
 
-//const app = express();
+const app = express();
 
 const config = {
-    host:'localhost',
+    host: 'localhost',
     user: 'SYNOVERGETECH\kachhahi',
-  //  password: '',
+    password: '',
     server: 'SVT-PC-193',
     database: 'Hitaxi',
     port: 8080
 };
+app.get('/', function (req, res) {
+    mssql.connect(config, function (err) {
 
-mssql.connect(config, function (err) {
+        if (err) console.log(err);
 
-    if (err) console.log(err);
+        // create Request object
+        const request = new mssql.Request();
 
-    // create Request object
-    const request = new sql.Request();
+        // query to the database and get the records
+        request.query('select TOP(10) [Employee] from [Hitaxi].[dbo].[abc]', function (err, recordset) {
 
-    // query to the database and get the records
-    request.query('select TOP(10) [Employee] from [Hitaxi].[dbo].[abc]', function (err, recordset) {
+            if (err) console.log(err)
 
-        if (err) console.log(err)
+            // send records as a response
+            res.send(recordset);
 
-        // send records as a response
-        console.log(recordset);
-
+        });
     });
 });
-
-// var server = app.listen(3000, function () {
-//     console.log('Server is running..');
-// });
+var server = app.listen(3000, function () {
+    console.log('Server is running..');
+});
