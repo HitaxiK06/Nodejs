769d2c4dbab1b040777eb91215041ee58d344a98
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-21 15:34:04 +0530

    commit

--- Mongo-db/src/db/mongoose.js
+++ Mongo-db/src/db/mongoose.js
@@ -5,48 +5,49 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('users', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
-
-const user = new User({
-    name: 'Hitaxi',
-    age: 22
-})
-
-user.save().then(() => {
-    console.log(user)
-}).catch((error) => {
-    console.log('Error!', error)
-})
-
-
-
-
-// const Task = mongoose.model('Task', {
-//     Description: {
-//         type: String
-//     },
-//     Update: {
-//         type: String
-//     },
+// const User = mongoose.model('users', {
 //     name: {
-//         type: String
+//         type: String,
+//         required: true
+//     },
+//     age: {
+//         type: Number
 //     }
 // })
-// const task = new Task({
-//     Description: 'Trial',
-//     Update: 'In Progress',
-//     name: 'Hirvita'
+
+// const user = new User({
+//    // name: 'Hitaxi',
+//     age: 22
 // })
 
-// task.save().then(() => {
-//     console.log(task)
+// user.save().then(() => {
+//     console.log(user)
 // }).catch((error) => {
 //     console.log('Error!', error)
 // })
+
+
+
+
+const Task = mongoose.model('Task-Manager', {
+    Description: {
+        type: String
+    },
+    Update: {
+        type: String
+    },
+    name: {
+        type: String
+    }
+})
+const task = new Task({
+    Description: 'Trial11',
+    Update: 'In Progress', 
+    name: 'Hirvita'
+})
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log('Error!', error)
+})
