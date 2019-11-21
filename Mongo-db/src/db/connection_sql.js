e67895dad2e45b660071cdd0db9564dfffeb2123
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-21 17:49:56 +0530

    commit

---
+++ Mongo-db/src/db/connection_sql.js
@@ -0,0 +1,35 @@
+const mssql = require('mssql')
+const express = require('express');
+
+const app = express();
+
+const config = {
+    host:'localhost',
+    user: 'SYNOVERGETECH\kachhahi',
+    password: 'Hitaxi.kachhadiya',
+    server: 'SVT-PC-193',
+    database: 'Hitaxi',
+    
+};
+
+mssql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    const request = new sql.Request();
+
+    // query to the database and get the records
+    request.query('select TOP(10) [Employee] from [Hitaxi].[dbo].[abc]', function (err, recordset) {
+
+        if (err) console.log(err)
+
+        // send records as a response
+        console.log(recordset);
+
+    });
+});
+
+// var server = app.listen(3000, function () {
+//     console.log('Server is running..');
+// });
--- Mongo-db/src/db/mongoose.js
+++ Mongo-db/src/db/mongoose.js
@@ -1,53 +1,80 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-// const User = mongoose.model('users', {
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-
-// const user = new User({
-//    // name: 'Hitaxi',
-//     age: 22
-// })
-
-// user.save().then(() => {
-//     console.log(user)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-
-
-
-const Task = mongoose.model('Task-Manager', {
-    Description: {
-        type: String
-    },
-    Update: {
-        type: String
-    },
+const User = mongoose.model('users', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a Positive')
+            }
+        }
+    },
+    email: {
+        type: String,
+        lowecase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email Invalid')
+            }
+        }
+    },
+    password : {
+        type: String,
+        required: true,
+        minlength:7,
+        // validate(value) {
+        //     if (!validator.isEmail(value)) {
+        //         throw new Error('Email Invalid')
+        //     }
+        // }
     }
 })
-const task = new Task({
-    Description: 'Trial11',
-    Update: 'In Progress', 
-    name: 'Hirvita'
+
+const user = new User({
+    name: 'Hitaxi Kachhadiya',
+    age: 22,
+    email:'hitaxi@synoverge.com',
+    password: 'Hitaxi12'
 })
 
-task.save().then(() => {
-    console.log(task)
+user.save().then(() => {
+    console.log(user)
 }).catch((error) => {
     console.log('Error!', error)
 })
+
+
+
+
+// const Task = mongoose.model('Task-Manager', {
+//     Description: {
+//         type: String
+//     },
+//     Update: {
+//         type: String
+//     },
+//     name: {
+//         type: String
+//     }
+// })
+// const task = new Task({
+//     Description: 'Trial11',
+//     Update: 'In Progress', 
+//     name: 'Hirvita'
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
