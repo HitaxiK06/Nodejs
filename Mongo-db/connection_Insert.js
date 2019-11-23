629b2929051b8c4685e669ee68ad1fe7d1219705
HitaxiK06 <hitaxi.kachhadiya@synoverge.com>
2019-11-23 11:48:39 +0530

    commit

--- Mongo-db/connection_Insert.js
+++ Mongo-db/connection_Insert.js
@@ -14,47 +14,47 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     const db = client.db(databaseName)
     //1.
-    db.collection('users').insertOne({
-        _id: Id,
-        name: 'Hirvita Rajyaguru',
+    // db.collection('users').insertOne({
+    //     _id: Id,
+    //     name: 'Hirvita Rajyaguru',
+    //     age: 23
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('unable to insert User')
+    //     }
+    //     console.log(result.ops)
+    // })
+    //2.
+    db.collection('users').insertMany([{
+        name: 'Adit Pandey',
+        age: 20
+    },
+    {
+        name: 'Basni Hadvani',
         age: 23
-    }, (error, result) => {
+    }
+    ], (error, result) => {
         if (error) {
             return console.log('unable to insert User')
         }
         console.log(result.ops)
     })
-    //2.
-    // db.collection('users').insertMany([{
-    //     name: 'Adit Pandey',
-    //     age: 20
-    // },
-    // {
-    //     name: 'Basni Hadvani',
-    //     age: 23
-    // }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('unable to insert User')
-    //     }
-    //     console.log(result.ops)
-    // })
-    // db.collection('Task').insertMany([{
-    //     Description: 'Insert Data',
-    //     Update: 'Completed'
-    // },
-    // {
-    //     name: 'Update Data',
-    // },
-    // {
-    //     name: 'Deleted Data',
-    //     Update: 'Incomplete'
-    // }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('unable to insert User')
-    //     }
-    //     console.log(result.ops)
-    // })
+    db.collection('Task').insertMany([{
+        Description: 'Insert Data',
+        Update: 'Completed'
+    },
+    {
+        name: 'Update Data',
+    },
+    {
+        name: 'Deleted Data',
+        Update: 'Incomplete'
+    }
+    ], (error, result) => {
+        if (error) {
+            return console.log('unable to insert User')
+        }
+        console.log(result.ops)
+    })
 
 })
--- Mongo-db/src/index.js
+++ Mongo-db/src/index.js
@@ -6,13 +6,38 @@ const app = express()
 const port = process.env.port || 3000
 app.use(express.json())
 
+//Create user 
 app.post('/users', (req, res) => {
-    // console.log(req.body)
-    // res.send('Trial')
+   
     const user = new User(req.body)
 
     user.save().then(() => {
         res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+//Get all users
+app.get('/users', (req, res) => {
+
+    User.find({}).then((user) => {
+        res.send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+//Get User by ID
+app.get('/users/:id', (req, res) => {
+
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
     }).catch((error) => {
         res.status(400).send(error)
     })
