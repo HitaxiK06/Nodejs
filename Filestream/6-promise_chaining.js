3853923b646376325664a84865d93878941d6f3a
HitaxiK06 <hitaxi.kachhadiya@synoverge.com>
2019-11-23 12:14:51 +0530

    commit

---
+++ Filestream/6-promise_chaining.js
@@ -0,0 +1,31 @@
+const add = (a, b) => {
+    return  new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 1000)
+    })
+}
+
+//1. Nested  Not ideal reason->catch multitime
+
+add(1, 2).then((sum) => {
+    add(sum, 2).then((sum2) => {
+        console.log(sum, sum2)
+    }).catch((e) => {
+        console.log(e)
+    })
+}).catch((e) => {
+    console.log(e)
+})
+
+//2. Nested Promise multiple Then
+
+add(1, 2).then((sum) => {
+        console.log(sum)
+        return add(sum,2)
+    
+}).then((sum2) => {
+    return add(sum2)
+}).catch((e) => {
+    console.log(e)
+})
