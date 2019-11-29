9a62e3f310b2265c0070e332fbf847ee0e226629
HitaxiK06 <56957868+HitaxiK06@users.noreply.github.com>
2019-11-29 14:57:16 +0530

    commit

--- Middleware/src/index.js
+++ Middleware/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/connection_mongoose')
 const userRouter = require('./router/user')
 const taskRouter = require('./router/tasks')
+const bcrypt = require('bcryptjs')  //for incription
 
 const app = express()
 const port = process.env.port || 3000
@@ -19,3 +20,11 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Serve is up..!')
 })
+
+const MyFunction = async() => {
+    const password = 'Hitaxi'
+    const hashpass = await bcrypt.hash(password,2)      //Hash function not reversable
+
+    console.log(hashpass)
+}
+MyFunction()
